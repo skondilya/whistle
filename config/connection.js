c3e2a9f9d4397acfd5c4f38a486ab8406dca1480
Louise Nyambati <louisen1993@gmail.com>
2017-05-10 19:29:03 -0500

    edited connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,23 @@
 
 var mysql = require("mysql");
+
+
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "",
+  password: "",
+  database: "whistle"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
