ab80e8857b728f7ada26c2d5b8d5c498cdb34481
skondilya <skondilya@gmail.com>
2017-05-13 15:15:29 -0500

    adding the position parameters

--- public/assets/javascript/map.js
+++ public/assets/javascript/map.js
@@ -18,19 +18,30 @@ function getPosition(position) {
     long = position.coords.longitude;
     console.log(long);
 }
-getLocation(lat,long);
 
+getLocation();
 
-function myMap() {
-   var mapOptions = {
-       center: new google.maps.LatLng(lat,long),
-       zoom: 10,
-       mapTypeId: google.maps.MapTypeId.HYBRID
-}
-  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+};
+// function myMap() {
+//    var mapOptions = {
+//        center: new google.maps.LatLng(lat,long),
+//        zoom: 10,
+//        mapTypeId: google.maps.MapTypeId.HYBRID
+//     }
+//   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-}
-myMap();
-
+// }
 });
+
+
 
