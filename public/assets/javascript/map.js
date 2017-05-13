35c93b058081976cc2f8ec90d20d9c58767b5db8
Louise Nyambati <louisen1993@gmail.com>
2017-05-13 12:47:34 -0500

    worked on map.js

--- public/assets/javascript/map.js
+++ public/assets/javascript/map.js
@@ -1,10 +1,12 @@
+
+
 var lat ="";
 var long ="";
 
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(getPosition);
-    } else { 
+    } else {
         console.log("Geolocation is not supported by this browser.");
     }
 }
@@ -16,19 +18,20 @@ function getPosition(position) {
     console.log(long);
 }
 
+    var queryURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDfbFj3x7Wjo4AnWFSJSWLLaArMrnEpZFw" +
+     "&AIzaSyDfbFj3x7Wjo4AnWFSJSWLLaArMrnEpZFw";
 
- function myMap() {
-    var mapOptions = {
-        center: new google.maps.LatLng(lat,long),
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.HYBRID
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(lat,long){
+      function myMap() {
+         var mapOptions = {
+             center: new google.maps.LatLng(lat,long),
+             zoom: 10,
+             mapTypeId: google.maps.MapTypeId.HYBRID
+         }
+         var map = new google.maps.Map(document.getElementById("map"), mapOptions);
     }
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  }
 
-myMap();
-
-
-
-
-
+});
