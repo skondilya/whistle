5d9dd3b052abcfa3abb8cb0adda4a19e611de07c
skondilya <skondilya@gmail.com>
2017-05-13 10:52:21 -0500

    adding the map.html and js file

--- public/assets/javascript/map.js
+++ public/assets/javascript/map.js
@@ -1,5 +1,5 @@
-
-      function initMap() {
+$(document).ready(function(){
+function initMap() {
         var uluru = {lat: -25.363, lng: 131.044};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
@@ -11,6 +11,5 @@
         });
       }
 
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDfbFj3x7Wjo4AnWFSJSWLLaArMrnEpZFw&callback=initMap">
-    </script>
+      
+});
