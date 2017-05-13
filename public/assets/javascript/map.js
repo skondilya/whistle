6527a207bd3b3c4cd7dcf5f3b1ebd2c9a0d2706d
skondilya <skondilya@gmail.com>
2017-05-13 12:19:47 -0500

    adding the getposition function

--- public/assets/javascript/map.js
+++ public/assets/javascript/map.js
@@ -1,8 +1,34 @@
+var lat ="";
+var long ="";
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(getPosition);
+    } else { 
+        console.log("Geolocation is not supported by this browser.");
+    }
+}
+
+function getPosition(position) {
+    lat =  position.coords.latitude ;
+    console.log(lat);
+    long =  position.coords.longitude;
+    console.log(long);
+}
+
+
  function myMap() {
     var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.12),
+        center: new google.maps.LatLng(lat,long),
         zoom: 10,
         mapTypeId: google.maps.MapTypeId.HYBRID
     }
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
   }
+
+myMap();
+
+
+
+
+
