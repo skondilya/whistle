792db016219a5f383efefc2d4d4ac0c7698fb6d3
skondilya <skondilya@gmail.com>
2017-05-13 13:31:53 -0500

    adding the map logic

--- public/assets/javascript/map.js
+++ public/assets/javascript/map.js
@@ -1,15 +1,13 @@
-
-
-var lat ="";
-var long ="";
-
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(getPosition);
-    } else {
+    } else { 
         console.log("Geolocation is not supported by this browser.");
     }
 }
+
+var lat ="";
+var long ="";
 
 function getPosition(position) {
     lat =  position.coords.latitude ;
@@ -18,20 +16,13 @@ function getPosition(position) {
     console.log(long);
 }
 
-    var queryURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDfbFj3x7Wjo4AnWFSJSWLLaArMrnEpZFw" +
-     "&AIzaSyDfbFj3x7Wjo4AnWFSJSWLLaArMrnEpZFw";
+function myMap() {
+     var mapOptions = {
+         center: new google.maps.LatLng(lat,long),
+         zoom: 10,
+         mapTypeId: google.maps.MapTypeId.HYBRID
+     }
+     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(lat,long){
-      function myMap() {
-         var mapOptions = {
-             center: new google.maps.LatLng(lat,long),
-             zoom: 10,
-             mapTypeId: google.maps.MapTypeId.HYBRID
-         }
-         var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    }
-
-});
+myMap();
