b50c37ba8c4a1a82267d5b6856b257b488f9baf2
Louise Nyambati <louisen1993@gmail.com>
2017-05-13 13:30:02 -0500

    edited map.js

--- public/assets/javascript/map.js
+++ public/assets/javascript/map.js
@@ -1,7 +1,6 @@
+$(document).ready(function(){
 
 
-var lat ="";
-var long ="";
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -10,28 +9,26 @@ function getLocation() {
         console.log("Geolocation is not supported by this browser.");
     }
 }
+var lat ="";
+var long ="";
 
 function getPosition(position) {
-    lat =  position.coords.latitude ;
+    lat = position.coords.latitude ;
     console.log(lat);
-    long =  position.coords.longitude;
+    long = position.coords.longitude;
     console.log(long);
 }
+getLocation(lat,long);
 
-    var queryURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDfbFj3x7Wjo4AnWFSJSWLLaArMrnEpZFw" +
-     "&AIzaSyDfbFj3x7Wjo4AnWFSJSWLLaArMrnEpZFw";
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(lat,long){
-      function myMap() {
-         var mapOptions = {
-             center: new google.maps.LatLng(lat,long),
-             zoom: 10,
-             mapTypeId: google.maps.MapTypeId.HYBRID
-         }
-         var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    }
+        function myMap() {
+           var mapOptions = {
+               center: new google.maps.LatLng(lat,long),
+               zoom: 10,
+               mapTypeId: google.maps.MapTypeId.HYBRID
+           }
+           var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+myMap();
+      }
 
-});
+  });
