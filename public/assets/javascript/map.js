a62db8d039d3ff3249ca1645595d893ecda74610
skondilya <skondilya@gmail.com>
2017-05-13 23:32:39 -0500

    adding the new myMap function

--- public/assets/javascript/map.js
+++ public/assets/javascript/map.js
@@ -3,62 +3,37 @@ $(document).ready(function(){
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(getPosition);
+        navigator.geolocation.getCurrentPosition(getPosition, errorfn);
     } else {
         console.log("Geolocation is not supported by this browser.");
     }
 }
 
-
-
 function getPosition(position) {
-  var lat = position.coords.latitude ;
+    var lat = position.coords.latitude ;
     console.log(lat);
-  var long = position.coords.longitude;
+    var long = position.coords.longitude;
     console.log(long);
-    return {lat:lat,long:long}
+    myMap(lat, long);
+}
+
+function errorfn()
+{
+  console.log("Users location cant be traced");
 }
 
 
-getLocation();
-
-function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-};
-// function myMap() {
-//    var mapOptions = {
-//        center: new google.maps.LatLng(lat,long),
-//        zoom: 10,
-//        mapTypeId: google.maps.MapTypeId.HYBRID
-//     }
-//   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-// }
-});
-
-<<<<<<< HEAD
-
-=======
-function myMap() {
-  var getcoordinates = getPostition(postition)
+function myMap(lat, long) {
+  console.log("the latitude is " + lat);
+  console.log("the longitude is " + long);
    var mapOptions = {
        center: new google.maps.LatLng(lat,long),
-       zoom: 10,
+       zoom: 1000,
        mapTypeId: google.maps.MapTypeId.HYBRID
 }
   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
 }
-myMap();
-
->>>>>>> f1c2ae7a4f05854eeb7c7c1255bc0fae8cddce21
+getLocation();
 
 });
