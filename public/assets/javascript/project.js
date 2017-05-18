0ccadd331d606e733fe68419bd3ac5e5a76d2f0a
Michael Dunner Weaver <Michael@Michaels-MacBook-Pro-2.local>
2017-05-17 20:49:04 -0500

    stuff in a branch

--- public/assets/javascript/project.js
+++ public/assets/javascript/project.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
+function sendMail() {
+ var link = "mailto:skondilya@gmail.com"
+         //+ "?cc=myCCaddress@example.com"
+            + "&subject=" + escape("I think I am in trouble")
+            + "&body=" + escape("Hey! Reach me at this location.I think i need your help.")
+           //+ escape(document.getElementById('myText').value)
+    ;
+ 
+   window.location.href = link;
+ }
+
+$(".policeimg").on("click", function() {
+    console.log("Send Email Button called");
+    // $.get("/api/email");  
+    sendMail({
+         // from: 'shreyakondilya@outlook.com',
+         // to: 'skondilya@gmail.com',
+         // subject: 'MailComposer sendmail',
+         // html: 'Mail of test sendmail '
+         // }, function (err, reply) {
+         // console.log(err && err.stack)
+         // console.dir(reply)
+    });
+});
 
 });
